09a0fe2c41da7a90ae60c6d32f877dd9c36184e4
Richard Chevalier <richard.chevalier@intel.com>
2016-06-02 15:54:47 +0200

    build: onetime_setup enhancement
    One currently need to setup a prject first for
    OPTION_FLASHTOOL_BINARY to be defined.
    Quick fix is to set a default value.
    
    Change-Id: I62002e5e3e305aaa77fb43a70f8a303e20acd4b8
    Tracked-On: https://jira.ndg.intel.com/browse/FIRE-6026
    Signed-off-by: Richard Chevalier <richard.chevalier@intel.com>

--- build/host/check.mk
+++ build/host/check.mk
@@ -26,6 +26,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+OPTION_FLASHTOOL_BINARY ?= platformflashtoollitecli
 include $(T)/build/Makefile.toolchain
 
 
